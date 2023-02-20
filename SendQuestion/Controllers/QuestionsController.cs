73158e80253294fbd952a220fe9af0beb1d5e99d
Richard Chalk <barmybrit@gmail.com>
2023-02-20 06:59:57 +0100

    Add project files.

---
+++ SendQuestion/Controllers/QuestionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendQuestion.Models;
+using SendQuestion.ViewModels;
+
+namespace SendQuestion.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendQuestion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendQuestion(QuestionViewModel questionVM)
+        {
+            if (ModelState.IsValid)
+            {
+                // This is where we can save to the database (if we had one!)
+                // _context.SaveChanges();
+
+                return RedirectToAction("QuestionConfirmation", questionVM);
+            }
+
+            return View(questionVM);
+        }
+
+        public IActionResult QuestionConfirmation(QuestionViewModel questionVM)
+        {
+            return View(questionVM);
+        }
+    }
+}
---
+++ SendQuestion/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SendQuestion.ViewModels
+{
+    public class QuestionViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+        
+        [Required]
+
+        public string Text { get; set; }
+    }
+}
